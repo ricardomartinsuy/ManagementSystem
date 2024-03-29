97115440b1aa7f7c664189b536847acdf7e2501a
ricardomartinsuy <77988270+ricardomartinsuy@users.noreply.github.com>
2024-03-29 12:53:42 -0300

    added the data to the tables

--- DatabaseProjectdb_managementsystem.sql
+++ DatabaseProjectdb_managementsystem.sql
@@ -64,3 +64,19 @@ END;
 
 GO
 
+
+INSERT INTO Products (Description, Brand, UnitOfMeasure, Photo, SupplierId)
+VALUES 
+    ('Camiseta', 'Nike', 'Unidade', 'caminho/para/foto1.jpg', 1),
+    ('Calça Jeans', 'Levis', 'Unidade', 'caminho/para/foto2.jpg', 2),
+    ('Tênis', 'Adidas', 'Unidade', 'caminho/para/foto3.jpg', 3),
+    ('Mochila', 'JanSport', 'Unidade', 'caminho/para/foto4.jpg', 4),
+    ('Boné', 'New Era', 'Unidade', 'caminho/para/foto5.jpg', 5);
+
+INSERT INTO Suppliers (Name, CNPJ, Address, Phone)
+VALUES 
+    ('Fornecedor 1', '12.345.678/0001-01', 'Rua A, 123', '(11) 1234-5678'),
+    ('Fornecedor 2', '98.765.432/0001-02', 'Rua B, 456', '(22) 9876-5432'),
+    ('Fornecedor 3', '23.456.789/0001-03', 'Rua C, 789', '(33) 2345-6789'),
+    ('Fornecedor 4', '45.678.901/0001-04', 'Rua D, 987', '(44) 5678-9012'),
+    ('Fornecedor 5', '67.890.123/0001-05', 'Rua E, 654', '(55) 6789-0123');
